e0d3021faa2dcb6ffc2f598b1aef67e2f3842e8e
Wayys <ntwayys@gmail.com>
2022-09-23 08:54:53 +0200

    updated css and bugfixes for chrome

--- index.js
+++ index.js
@@ -20,3 +20,25 @@ const activate = (targetIdEles) => {
     item.classList.add("active");
   }
 };
+
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const userEmailPhone = document.getElementById("user-email").value;
+  if (userEmailPhone) {
+    console.log("click");
+
+    // emailjs
+    //   .send("service_evc9c99", "template_75fwo6f", {
+    //     from_name: userEmailPhone,
+    //   })
+    //   .then(
+    //     function (response) {
+    //       console.log("SUCCESS!", response.status, response.text);
+    //     },
+    //     function (error) {
+    //       console.log("FAILED...", error);
+    //     }
+    //   );
+    document.getElementById("submit-btn").style.display = "none";
+    document.getElementById("submited-text").style.display = "block";
+  }
+});
